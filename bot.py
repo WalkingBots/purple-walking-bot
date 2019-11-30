813f437b3950c5e1b000acc14569a872881679cb
Saneck <47082880+Saneck@users.noreply.github.com>
2019-11-30 17:26:22 +0300

    all code saved

--- bot.py
+++ bot.py
@@ -11,6 +11,8 @@ bot = Bot(config.token, request=req)
 upd = Updater(bot=bot, use_context=True)
 dp = upd.dispatcher
 
+last_photo = None
+
 # логирование
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -19,7 +21,44 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # приветственное сообщение
 def hello(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Hello! My name is {0}".format(config.name))
+                             text="Привет, я бот Ева!".format(config.name))
+    request_photo(context.bot,chat_id=update.effective_chat.id)
+
+
+def request_photo(bot,chat_id):
+    bot.send_message(chat_id=chat_id,
+                     text="Пришли мне пожалуйста, фотографию :)")
+
+
+
+def request_location(chat_id):
+    location_keyboard = KeyboardButton(text="Send location", request_location=True)
+    custom_keyboard = [[ location_keyboard ]]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    bot.send_message(chat_id=chat_id,
+                     text="Пришли мне пожалуйста, свое местоположение :)",
+                     reply_markup=reply_markup)
+
+
+def location_callback(update, context):
+    dump.data_with_location('photo', bot.get_file(last_photo), update.message.from_user.username,
+                            update.message.location)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Спасибо! А эта фоточка для вас:")
+
+
+def photo_callback(update, context):
+    global last_photo
+    last_photo = update.message.photo[-1].file_id
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Спасибо за фотографию. Она прекрасна.")
+    request_location(update.effective_chat.id)
+
+
+mh = MessageHandler(Filters.location, location_callback)
+ph = MessageHandler(Filters.photo, photo_callback)
+dp.add_handler(mh)
+dp.add_handler(ph)
 
 # добавляем приветственное сообщение при команде старт
 dp.add_handler(CommandHandler('start', hello))
--- dump.py
+++ dump.py
@@ -3,25 +3,60 @@ import json
 import time
 from datetime import datetime
 import os
+import csv
 
 datadir = os.path.join(os.path.expanduser('~'), 'data', config.name.lower().replace(' ', '_'))
 if not os.path.exists(datadir): os.makedirs(datadir)
 
+with open(os.path.join(datadir,'dump.csv'), mode='w') as csv_file:
+    logf = csv.writer(csv_file)
+    logf.writerow(["id","longitude","latitude","timestamp","filetype","username"])
+    
 def fnpath(username, ts, ft, fmt):
     return os.path.join(datadir, '{0}_{1}_{2}.{3}'.format(username, ts, ft, fmt))
-
+    
+#locf = open(fnpath(username, ts, filetype, 'csv'), 'w')
 def data_with_location(filetype, data, username, location):
-    print('going to save data', filetype, data, username, location)
-    ts = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
-    if filetype == 'photo':
-        data.download(fnpath(username, ts, filetype, 'jpg'))
-    elif filetype == 'voice':
-        data.download(fnpath(username, ts, filetype, 'ogg'))
-    elif filetype == 'text':
-        with open(fnpath(username, ts, filetype, 'txt')) as textf:
-            textf.write(data)
-    else:
-        raise TypeError('wrong filetype')
-    with open(fnpath(username, ts, filetype, 'json'), 'w') as locf:
-        locf.write(json.dumps({'longitude': location.longitude,
-                               'latitude': location.latitude}, indent=2))
+#    logf = open(os.path.join(datadir,'dump.csv'), 'a')
+    with open(os.path.join(datadir,'dump.csv'), mode='a') as csv_file:
+        logf = csv.writer(csv_file)
+   
+        print('going to save data', filetype, data, username, location)
+        ts = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
+        if filetype == 'photo':
+            f = fnpath(username, ts, filetype, 'jpg')
+            data.download(f)
+        elif filetype == 'voice':
+            f = fnpath(username, ts, filetype, 'ogg')   
+            data.download(f)
+        elif filetype == 'text':
+            f = fnpath(username, ts, filetype, 'txt')
+            with open(f) as textf:
+                textf.write(data)
+        else:
+            f = ''
+            raise TypeError('wrong filetype')
+    #        locf.write(json.dumps({'filename': f,
+    #                               'longitude': location.longitude,
+    #                               'latitude': location.latitude,
+    #                               'timestamp': ts,
+    #                               'filetype': filetype,
+    #                               'username': username}, indent=2))
+        logf.writerow([str(f), str(location.longitude), str(location.latitude), str(ts), str(filetype), str(username)])
+        csv_file.flush()
+    
+def voice_source_peaks_location(voice, source, peaks, location, username):
+    with open(os.path.join(datadir,'dump.csv'), mode='a') as csv_file:
+        logf = csv.writer(csv_file)
+        print('going to save data', filetype, data, username, location)
+        ts = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
+        f = fnpath(username, ts, 'voice', 'ogg')    
+        data.download(f)
+    #        locf.write(json.dumps({'filename': f,s
+    #                               'longitude': location.longitude,
+    #                               'latitude': location.latitude,
+    #                               'timestamp': ts,
+    #                               'filetype': filetype,
+    #                               'username': username}, indent=2))
+        logf.writerow([str(f), str(location.longitude), str(location.latitude), str(ts), str(filetype), source, peaks, str(username)])
+        csv_file.flush()
